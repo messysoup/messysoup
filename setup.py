92ea77fe2adef455872b8984b1b8b9af42a95c8f
messysoup <messysoup@protonmail.com>
2022-01-04 18:57:45 -0500

    update build tools

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "readme.md").read_text()
 
 setup(
     name="messysoup",
-    version="0.0.1",
+    version="0.0.2",
     description="A wrapper for html",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -17,9 +17,13 @@ setup(
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",    
         "Programming Language :: Python :: 3.10",
     ],
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests', '*.tests', '*.tests.*']),
     include_package_data=True,
 )
