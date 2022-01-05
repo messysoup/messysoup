4e4a968414b56a822e360b3c3069e5fca95b2165
messysoup <messysoup@protonmail.com>
2022-01-04 19:21:08 -0500

    Updated readme to include experimental pyodide support

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "readme.md").read_text()
 
 setup(
     name="messysoup",
-    version="0.0.2",
+    version="0.0.3",
     description="A wrapper for html",
     long_description=README,
     long_description_content_type="text/markdown",
